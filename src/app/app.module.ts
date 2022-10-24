3c02c857dfa5f6f1fab5dec6ac363f963f4965de
felhe <10731299+felhe@users.noreply.github.com>
2022-10-24 22:28:41 +0200

    feat(keymap): use modal for settings on small screens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,42 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {de_DE, NZ_I18N} from 'ng-zorro-antd/i18n';
-import {registerLocaleData} from '@angular/common';
+import { AppComponent } from './app.component';
+import { de_DE, NZ_I18N } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
 import de from '@angular/common/locales/de';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {KeymapComponent} from './components/keymap/keymap.component';
-import {NzCardModule} from "ng-zorro-antd/card";
-import {NzTypographyModule} from "ng-zorro-antd/typography";
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { KeymapComponent } from './components/keymap/keymap.component';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 registerLocaleData(de);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    KeymapComponent
-  ],
+  declarations: [AppComponent, KeymapComponent],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
     NzCardModule,
-    NzTypographyModule
+    NzTypographyModule,
+    NzButtonModule,
+    NzIconModule,
+    NzDrawerModule,
+    NzModalModule,
+    NzSwitchModule,
+    NzGridModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: de_DE }],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
